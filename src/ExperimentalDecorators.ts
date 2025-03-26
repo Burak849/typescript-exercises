084b2c6c0a796ca537e68f7ba8077e3fe5dc6888
Burak849 <burakkurtulus018@gmail.com>
2025-03-26 04:02:41 +0300

    Experimental Decorators

--- src/ExperimentalDecorators.ts
+++ src/ExperimentalDecorators.ts
@@ -1,3 +1,4 @@
+import { title } from "node:process";
 
 function Logger( logString: string ) {
     console.log('Logger Factory');
@@ -7,18 +8,32 @@ function Logger( logString: string ) {
     };
 }
 
+//! CHECK THOSE AGAIN
+
 function WithTemplate( template: string , hookId: string ){
+
     console.log('Template Factory');
-    return function ( constructor : any ){ // this is the actual decorator
-        console.log('Rendering template!');
-        const hookEl = document.getElementById(hookId);
-        const p = new constructor ();
-        if ( hookEl ){
-            hookEl.innerHTML = template;
-            hookEl.querySelector('h1')!.textContent = p.name;
-        }
-    }
+    return function<T extends { new(...args: any[]): { name: string } }>( originalConstructor : T ){ // this is the actual decorator
+        
+        
+        
+        return class extends originalConstructor {
+            constructor( ..._: any[] ){ // when you put _ you say to typescript that i know i didnt use but i dont need
+                super();
+                console.log('Rendering template!');
+                const hookEl = document.getElementById(hookId);
+                const p = new originalConstructor ();
+                if ( hookEl ){
+                    hookEl.innerHTML = template;
+                    hookEl.querySelector('h1')!.textContent = p.name;
+                }
+
+            }
+        } 
     
+    
+    
+    };
 }
 
 @Logger('LOGGING') // render later
@@ -45,7 +60,6 @@ function Log2 ( target: any , name: string , descriptor: PropertyDescriptor ){
     console.log( target );
     console.log( name );
     console.log( descriptor );
-    
 }
 
 function Log3 ( target: any, name: string | symbol , descriptor : PropertyDescriptor ){
@@ -89,3 +103,107 @@ class Product {
 
 const p1 = new Product('Book', 19);
 const p2 = new Product('Book2', 29);
+
+function Autobind( _: any, _2: string , descriptor: PropertyDescriptor ){
+    const originalMethod = descriptor.value;
+    const adjDescriptor: PropertyDescriptor = {
+        configurable: true,
+        enumerable: false,
+        get(){
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        }
+    };
+    return adjDescriptor;
+}
+
+class Printer {
+    message = 'This works!';
+    
+
+    @Autobind
+    showMessage(){
+        console.log(this.message);
+    }
+}
+
+const p = new Printer();
+const button = document.querySelector('button')!; 
+button?.addEventListener('click', p.showMessage ); // by adding .bind(p) it will change the message
+
+interface ValidatorConfig{
+    [property: string] : {
+        [validateableProps: string]: string[]; // ['required', 'positive']
+    }
+}
+
+const registeredValidators: ValidatorConfig = {};
+
+function Required( target: any , propName: string ){
+    registeredValidators[target.constructor.name] = { // constructor used to create our object we can use any function with this
+        ...registeredValidators[target.constructor.name],
+        [propName]: [...(registeredValidators[target.constructor.name]?.[propName] ?? []), 'required']
+    }; 
+}
+function PositiveNumber( target: any , propName: string ){
+    registeredValidators[target.constructor.name] = { 
+        ...registeredValidators[target.constructor.name],
+        [propName]: [...(registeredValidators[target.constructor.name]?.[propName] ?? []), 'positive']
+    };
+}
+function validate( obj: any ){ // this function must work with all the validators 
+    const objValidatorConfig = registeredValidators[obj.constructor.name];
+    if (!objValidatorConfig){
+        return true;
+    }
+
+    let isValid = true;
+
+    for( const prop in objValidatorConfig ){
+        for ( const validator of objValidatorConfig[prop] ){
+            switch ( validator ){
+
+                case 'required': 
+                isValid = isValid && !!obj[prop]; // boolean type
+                break;
+
+                case 'positive':
+                isValid = isValid && obj[prop] > 0;
+                break;
+                
+            }
+        }
+    }
+    return isValid;
+}
+
+class Course {
+    @Required
+    title: string;
+    @PositiveNumber
+    price: number;
+    constructor ( t: string, p: number ){
+        this.title = t;
+        this.price = p;
+    }
+}
+
+const courseForm = document.querySelector('form');
+courseForm?.addEventListener('submit', event => { 
+    event.preventDefault();
+    const titleEl = document.getElementById('title') as HTMLInputElement;
+    const priceEl = document.getElementById('price') as HTMLInputElement;
+
+    const title = titleEl.value;
+    const price = +priceEl.value;
+
+    // now we can make the validation with decorators because we are saving also empty inputs
+
+    const createdCourse = new Course( title, price );
+
+    if( !validate(createdCourse) ){
+        alert('Invalid input, please try again.');
+        return;
+    }
+    console.log(createdCourse);
+})
